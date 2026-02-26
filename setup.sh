d255e01eaa2aa3b44e783ea2f87c882a096e2cb1
Kaks753 <musyokas753@gmail.com>
2026-02-26 11:48:44 +0300

    streamlit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Installing requirements..."
+pip install --no-cache-dir streamlit
+pip install --no-cache-dir plotly
+pip install --no-cache-dir pandas
+pip install --no-cache-dir numpy
+echo "Installation complete!"
